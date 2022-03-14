8f7960d5e725cf9f161d64683b387ef8fd299be2
gloriahu <43248664+gloriahu@users.noreply.github.com>
2022-03-14 08:43:00 -0400

    Add files via upload

---
+++ ImbalancedLearningRegression/test_Tomeklinks.py
@@ -0,0 +1,26 @@
+## load libraries
+import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
+import ssl
+
+from tomeklinks import tomeklinks
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+college = pd.read_csv(
+        ## http://jse.amstat.org/v19n3/decock.pdf
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearningRegression/master/data/College.csv"
+)
+
+college_tomek = tomeklinks(
+
+    ## main arguments
+    data = college,  ## pandas dataframe
+    y = "Grad.Rate",  ## string ('header name')
+)
+college_tomek.reset_index(inplace=True)
+
+seaborn.kdeplot(college["Grad.Rate"], label="Original")
+seaborn.kdeplot(college_tomek["Grad.Rate"], label="Modified")
+plt.show()
