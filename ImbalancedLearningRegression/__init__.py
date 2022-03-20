18edc887527c1a9925eecca0ff9f46186bd66027
wuwenglei <wwu077@uottawa.ca>
2022-03-20 18:49:33 -0400

    Initialize __init__.py

--- ImbalancedLearningRegression/__init__.py
+++ ImbalancedLearningRegression/__init__.py
@@ -1,22 +1,31 @@
 # __init__.py
 
-# Version of the ImbalancedLearn-Regression package
+# Version of the ImbalancedLearningRegression package
 __version__ = "0.0.0"
 
 """
-Imbalanced Learn for Regression
-https://github.com/paobranco/ImbalancedLearn-Regression
+Imbalanced Learning for Regression
+https://github.com/paobranco/ImbalancedLearningRegression
 """
 
-# from smogn.box_plot_stats import box_plot_stats
-# from smogn.phi_ctrl_pts import phi_ctrl_pts
-# from smogn.phi import phi
-# from smogn.smoter import smoter
+from ImbalancedLearningRegression.box_plot_stats import box_plot_stats
+from ImbalancedLearningRegression.phi import phi
+from ImbalancedLearningRegression.phi_ctrl_pts import phi_ctrl_pts
+from ImbalancedLearningRegression.over_sampling_gn import over_sampling_gn
+from ImbalancedLearningRegression.over_sampling_ro import over_sampling_ro
+from ImbalancedLearningRegression.under_sampling_cnn import under_sampling_cnn
+from ImbalancedLearningRegression.under_sampling_enn import under_sampling_enn
+
+
+
 
 __all__ = [
 
-    # "box_plot_stats",
-    # "phi_ctrl_pts",
-    # "phi",
-    # "smoter"
+    "box_plot_stats",
+    "phi_ctrl_pts",
+    "phi",
+    "over_sampling_gn",
+    "over_sampling_ro",
+    "under_sampling_cnn",
+    "under_sampling_enn"
 ]
