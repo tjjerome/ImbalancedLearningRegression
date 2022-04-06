9bc3617ffefdd76d399b4f72803585bb5438566d
gloriahu <43248664+gloriahu@users.noreply.github.com>
2022-04-06 19:11:47 -0400

    Update test_random_under.py

--- ImbalancedLearningRegression/test_random_under.py
+++ ImbalancedLearningRegression/test_random_under.py
@@ -4,23 +4,22 @@ import seaborn
 import matplotlib.pyplot as plt
 import ssl
 
-from random_under import random_under
+from ImbalancedLearningRegression.random_under import random_under
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-housing = pd.read_csv(
-    ## http://jse.amstat.org/v19n3/decock.pdf
-    'https://raw.githubusercontent.com/nickkunz/smogn/master/data/housing.csv'
+college = pd.read_csv(
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearningRegression/master/data/College.csv"
 )
 
-housing_ru = random_under(
-
+college_ru = random_under(
     ## main arguments
-    data = housing,  ## pandas dataframe
-    y = "SalePrice",  ## string ('header name')
+    data=college,  ## pandas dataframe
+    y='Grad.Rate'  ## string ('header name')
 )
-housing_ru.reset_index(inplace=True)
 
-seaborn.kdeplot(housing["SalePrice"], label="Original")
-seaborn.kdeplot(housing_ru["SalePrice"], label="Modified")
+college_ru.reset_index(inplace=True)
+
+seaborn.kdeplot(college['Grad.Rate'], label="Original")
+seaborn.kdeplot(college_ru['Grad.Rate'], label="Modified")
 plt.show()
