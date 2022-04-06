3a309044188aab9b6fff4a7a88f93a1bb146fdc1
gloriahu <43248664+gloriahu@users.noreply.github.com>
2022-04-06 19:06:22 -0400

    Update test_adasyn.py

--- ImbalancedLearningRegression/test_adasyn.py
+++ ImbalancedLearningRegression/test_adasyn.py
@@ -4,19 +4,18 @@ import seaborn
 import matplotlib.pyplot as plt
 import ssl
 
-from adasyn import adasyn
+from ImbalancedLearningRegression.adasyn import adasyn
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 college = pd.read_csv(
-        ## http://jse.amstat.org/v19n3/decock.pdf
-    "https://raw.githubusercontent.com/paobranco/ImbalancedLearnRegression/master/data/College.csv"
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearningRegression/master/data/College.csv"
 )
 
 college_adasyn = adasyn(
     ## main arguments
     data=college,  ## pandas dataframe
-    y='Grad.Rate',  ## string ('header name')
+    y='Grad.Rate'  ## string ('header name')
 )
 college_adasyn.reset_index(inplace=True)
 
